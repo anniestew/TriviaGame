d3fa1d7e8f3490c75901137c7c4ac0fd50708c8d
Annie Stewart <anniestewart@Annies-MacBook-Air.local>
2019-04-06 15:48:17 -0400

    index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,35 @@
 //TRIVIA GAME
-
-var timeLeft = 100;
-var timer = setInterval(function(){
-  document.getElementById("countdown").innerHTML = timeLeft + " seconds remaining";
-  timeLeft -= 1;
-  if(timeLeft <= 0){
-    clearInterval(timer);
-    document.getElementById("countdown").innerHTML = "Time's Up!"
-  }
+function countdown () {
+  var timeLeft = 100;
+  var timer = setInterval(function(){
+    document.getElementById("countdown").innerHTML = timeLeft + " seconds remaining";
+    timeLeft -= 1;
+    if(timeLeft <= 0){
+      clearInterval(timer);
+      document.getElementById("countdown").innerHTML = "Time's Up!"
+    }
 }, 1000);
+}
+
+$(document).ready(function(){
+
+	$("trivia-container").hide();
+	$("#finish-container").hide();
+
+
+$("#startButton").on("click", function(){
+
+  // Hide the start Div from the user
+  $("#startGame-container").hide();
+
+
+  // Show the Game Div
+  $("#trivia-container").show();
+
+  // startCountdown();
+  // return;
+
+});
 
 // // Variable to hold the index of current question.
 // // var questionIndex = 0;
